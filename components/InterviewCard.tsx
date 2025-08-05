05e2191dd1ac140533ac64d9a7d6ce6ebec8d4e1
Anubhavrawat18 <anubhavrawat444@gmail.com>
2025-08-05 23:55:42 +0530

    styles

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -23,7 +23,7 @@ const InterviewCard = ({
     <div className="card-border w-[360px] max-sm:w-full min-h-96">
       <div className="card-interview">
         <div>
-          <div className="absolute top-0 right-0 w-fit px-4 py-2 rounded-bl-lg bg-light-600">
+          <div className="absolute top-0 right-0 w-fit px-4 py-2 rounded-bl-lg bg-light-400">
             <p className="badge-text">{normalizedType}</p>
           </div>
           <Image
